fc21370a0eee9f12798782ceb0aab29f190744c7
Elyor_Umirzoqov <elyorumirzoqov1.gmail.com>
2021-11-07 12:17:58 +0500

    bismillah

---
+++ app.js
@@ -0,0 +1,14 @@
+let options = {
+  strings: [
+    "Hech kim sendan kuchli emas,",
+    "Faqat ular oldinroq boshlagan",
+    "Stiv Jobs",
+  ],
+  typeSpeed: 60,
+  startDelay: 500,
+  backSpeed: 40,
+  backDelay: 1000,
+  loop: true,
+  loopCount: Infinity,
+};
+let typed = new Typed(".element", options);
